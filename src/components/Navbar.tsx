49c6d9ff48bada04a5cf8c582e6aa8f31a1e9b8f
Samarth Kapoor <kapoorsamarth7@gmail.com>
2025-01-10 23:43:57 +0530

    completed Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 
 const Navbar = () => {
     return (
-
+        <nav className="max-w-7xl mx-auto py-4 px-4 flex justify-between items-center">
+            <Link href="/">
+               <h1 className="text-2xl font-extrabold text-lime-500 tracking-tighter">
+                bimal
+               </h1>
+            </Link>
+            <div className="flex items-center gap-4">
+                <Link
+                  href="#products"
+                  className="text-lime-950 font-medium tracking-tight hover:text-lime-900 transition-all duration-300"
+                >
+                    Products
+                </Link>
+                <button className="bg-lime-600 text-white font-medium px-4 py-2 hover:text-lime-700 transition-all duration-300 rounded-lg">
+                    Login
+                </button>
+            </div>
+        </nav>
     );
 };
 
