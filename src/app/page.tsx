f31aaec7e752ea5a078f31fb20a6808e0ef6f75c
Samarth Kapoor <kapoorsamarth7@gmail.com>
2025-01-11 18:37:44 +0530

    finished page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Content from "@/components/Content";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
@@ -7,6 +9,8 @@ export default function Home() {
       <main>
           <Navbar />
           <Hero />
+          <Content />
+          <Footer />
       </main>
   )
 }
