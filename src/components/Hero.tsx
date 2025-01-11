49553f2e297d8a668e77fd2a9b24d7a093b86309
Samarth Kapoor <kapoorsamarth7@gmail.com>
2025-01-11 18:39:16 +0530

    added responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,10 @@ const Hero = () => {
     return (
         <section className="max-w-7xl mx-auto pt-12 px-4 flex flex-col items-center justify-center">
             <div className="flex flex-col items-center justify-center pt-8">
-                <h1 className="text-6xl font-extrabold text-lime-950 tracking-tighter">
+                <h1 className="md:text-6xl text-4xl font-extrabold text-lime-950 tracking-tighter">
                     Bolo Zubaan <span className="text-lime-50">Canceri</span>
                 </h1>
-                <p className="text-xl text-lime-950">
+                <p className="md:text-xl text-lg text-lime-950">
                   More you eats, sooner you dies  
                 </p>
             </div>
@@ -17,7 +17,7 @@ const Hero = () => {
                alt="Bolo Zubaan Canceri"
                width={500}
                height={500}
-               className="w-[40%] h-full object-contain"
+               className="w-[80%] md:w-[40%] h-full object-contain"
                unoptimized
             />
         </section>
